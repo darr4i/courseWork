4091e5825425789d1f56045749441c2c4d958b8b
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2023-05-20 16:20:15 +0300

    add function createBoard

--- game.js
+++ game.js
@@ -1,2 +1,13 @@
 const gameBoard = document.querySelector("#gameboard");
 const info = document.querySelector("#info");
+
+const cells = [
+    "", "", "", "", "", "", "", "", "",
+]
+
+const createBoard = () => {
+cells.forEach((cell, index) => {
+    const cellElement = document.createElement('div')
+    cellElement.classList.add('square');
+})
+}
