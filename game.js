28f33f1082d8bec58c9d45470e76ee6e50afe7ea
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2023-05-24 14:02:35 +0300

    fixed bags

--- game.js
+++ game.js
@@ -16,6 +16,7 @@ const nextMove = (e) => {
     go = go === "cross" ? "circle" : "cross";
     info.textContent = "it is " + go + " now";
     e.target.removeEventListener("click", nextMove);
+    checkScore();
 }
 
 const createBoard = () => {
@@ -27,10 +28,9 @@ cells.forEach((cell, index) => {
     gameBoard.append(cellElement);
  })
 }
-createBoard();
 
 const checkScore = () => {
-    const allSquares = document.querySelector(".square");
+    const allSquares = document.querySelectorAll(".square");
     const combo = [
         [0,1,2], [3,4,5], [6,7,8],
         [0,3,6], [1,4,7], [2,5,8],
@@ -38,20 +38,25 @@ const checkScore = () => {
     ]
 
     combo.forEach(array => {
-        const crossWins = array.every(cell => allSquares[cell].firstChild?.classList.contains("cross"));
+        const crossWins = array.every(cell => allSquares[cell].firstChild?.classList.contains('cross'));
+
         if (crossWins) {
             info.textContent = "Cross wins!"
             allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
-            return crossWins
+            return
         }
     })
 
+    console.log(allSquares[0]);
+
     combo.forEach(array => {
-        const circleWins = array.every(cell => allSquares[cell].firstChild?.classList.contains("circle"));
+        const circleWins = array.every(cell => allSquares[cell].firstChild?.classList.contains('circle'));
+
         if (circleWins) {
             info.textContent = "Circle wins!"
             allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
-            return circleWins
+            return
         }
     })
 }
+createBoard();
